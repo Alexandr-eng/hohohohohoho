dc3bcf0919c12f2e0bffefe97ea9eee72451e4bb
frutez <glebka.yatsenko@gmail.com>
2024-01-02 00:34:44 +0300

    add: @action products for warehouse

--- myproject/users/serializers.py
+++ myproject/users/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from .models import User, Warehouse, Product, Basket
 
+
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'type']
+        fields = ['id', 'username', 'email', 'type', 'password']
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User(username=validated_data['username'],email=validated_data['email'],type=validated_data['type'])
+        user.set_password(validated_data['password'])
+        user.save(update_fields=['password'])
+        return user
+
 
 class WarehouseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Warehouse
         fields = ['id', 'name']
+
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
