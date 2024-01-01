004d86a350a413e9b266146fa6df262e32e82b71
frutez <glebka.yatsenko@gmail.com>
2024-01-01 19:31:07 +0300

    Add: basket доработать модель пользователя

--- myproject/users/models.py
+++ myproject/users/models.py
@@ -28,3 +28,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return f'Basket for {self.user.username} : Product {self.product.name}'
